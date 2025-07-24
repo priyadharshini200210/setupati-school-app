4e7714d4278c897821f1f0c52768f7ba72e52427
Abarna <727821tuec001@skct.edu.in>
2025-07-24 15:39:09 +0530

    winston logger implemented

---
+++ src/logger.js
@@ -0,0 +1,15 @@
+import { createLogger, format, transports } from 'winston';
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp(),
+    format.colorize(),
+    format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
+  ),
+  transports: [
+    new transports.Console(), 
+  ],
+});
+
+export default logger;
