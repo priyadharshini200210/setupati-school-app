993eb3baa2e5acc7eeda9366981fdf45af253c5c
Vijayasri M <vijayasrimariappan@gmail.com>
2025-07-25 01:43:37 +0530

    fix colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,63 +20,63 @@ export default {
 			}
 		},
 		extend: {
-			// colors: {
-			// 	border: 'hsl(var(--border))',
-			// 	input: 'hsl(var(--input))',
-			// 	ring: 'hsl(var(--ring))',
-			// 	background: 'hsl(var(--background))',
-			// 	foreground: 'hsl(var(--foreground))',
-			// 	primary: {
-			// 		DEFAULT: 'hsl(var(--primary))',
-			// 		foreground: 'hsl(var(--primary-foreground))',
-			// 		soft: 'hsl(var(--primary-soft))'
-			// 	},
-			// 	secondary: {
-			// 		DEFAULT: 'hsl(var(--secondary))',
-			// 		foreground: 'hsl(var(--secondary-foreground))'
-			// 	},
-			// 	destructive: {
-			// 		DEFAULT: 'hsl(var(--destructive))',
-			// 		foreground: 'hsl(var(--destructive-foreground))'
-			// 	},
-			// 	muted: {
-			// 		DEFAULT: 'hsl(var(--muted))',
-			// 		foreground: 'hsl(var(--muted-foreground))'
-			// 	},
-			// 	accent: {
-			// 		DEFAULT: 'hsl(var(--accent))',
-			// 		foreground: 'hsl(var(--accent-foreground))',
-			// 		strong: 'hsl(var(--accent-strong))'
-			// 	},
-			// 	popover: {
-			// 		DEFAULT: 'hsl(var(--popover))',
-			// 		foreground: 'hsl(var(--popover-foreground))'
-			// 	},
-			// 	card: {
-			// 		DEFAULT: 'hsl(var(--card))',
-			// 		foreground: 'hsl(var(--card-foreground))'
-			// 	},
-			// 	success: {
-			// 		DEFAULT: 'hsl(var(--success))',
-			// 		foreground: 'hsl(var(--success-foreground))',
-			// 		soft: 'hsl(var(--success-soft))'
-			// 	},
-			// 	warning: {
-			// 		DEFAULT: 'hsl(var(--warning))',
-			// 		foreground: 'hsl(var(--warning-foreground))',
-			// 		soft: 'hsl(var(--warning-soft))'
-			// 	},
-			// 	sidebar: {
-			// 		DEFAULT: 'hsl(var(--sidebar-background))',
-			// 		foreground: 'hsl(var(--sidebar-foreground))',
-			// 		primary: 'hsl(var(--sidebar-primary))',
-			// 		'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
-			// 		accent: 'hsl(var(--sidebar-accent))',
-			// 		'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-			// 		border: 'hsl(var(--sidebar-border))',
-			// 		ring: 'hsl(var(--sidebar-ring))'
-			// 	}
-			// },
+			colors: {
+				border: 'hsl(var(--border))',
+				input: 'hsl(var(--input))',
+				ring: 'hsl(var(--ring))',
+				background: 'hsl(var(--background))',
+				foreground: 'hsl(var(--foreground))',
+				primary: {
+					DEFAULT: 'hsl(var(--primary))',
+					foreground: 'hsl(var(--primary-foreground))',
+					soft: 'hsl(var(--primary-soft))'
+				},
+				secondary: {
+					DEFAULT: 'hsl(var(--secondary))',
+					foreground: 'hsl(var(--secondary-foreground))'
+				},
+				destructive: {
+					DEFAULT: 'hsl(var(--destructive))',
+					foreground: 'hsl(var(--destructive-foreground))'
+				},
+				muted: {
+					DEFAULT: 'hsl(var(--muted))',
+					foreground: 'hsl(var(--muted-foreground))'
+				},
+				accent: {
+					DEFAULT: 'hsl(var(--accent))',
+					foreground: 'hsl(var(--accent-foreground))',
+					strong: 'hsl(var(--accent-strong))'
+				},
+				popover: {
+					DEFAULT: 'hsl(var(--popover))',
+					foreground: 'hsl(var(--popover-foreground))'
+				},
+				card: {
+					DEFAULT: 'hsl(var(--card))',
+					foreground: 'hsl(var(--card-foreground))'
+				},
+				success: {
+					DEFAULT: 'hsl(var(--success))',
+					foreground: 'hsl(var(--success-foreground))',
+					soft: 'hsl(var(--success-soft))'
+				},
+				warning: {
+					DEFAULT: 'hsl(var(--warning))',
+					foreground: 'hsl(var(--warning-foreground))',
+					soft: 'hsl(var(--warning-soft))'
+				},
+				sidebar: {
+					DEFAULT: 'hsl(var(--sidebar-background))',
+					foreground: 'hsl(var(--sidebar-foreground))',
+					primary: 'hsl(var(--sidebar-primary))',
+					'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+					accent: 'hsl(var(--sidebar-accent))',
+					'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+					border: 'hsl(var(--sidebar-border))',
+					ring: 'hsl(var(--sidebar-ring))'
+				}
+			},
 			borderRadius: {
 				lg: 'var(--radius)',
 				md: 'calc(var(--radius) - 2px)',
