f9cd2d8a923e539d798818ade1960dde6b18358e
SHYAM-001 <shyamkathir007@gmail.com>
2025-10-26 19:51:05 +0530

    Changed the import path

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -3,9 +3,9 @@ import { Sidebar } from './Sidebar';
 import { Header } from './Header';
 import { initializeSampleData } from '@/store/schoolStore';
 import { useAuthStore } from '@/store/authStore';
-import { StudentDashboard } from '@/components/students/StudentDashboard';
-import { TeacherDashboard } from '@/components/teachers/TeacherDashboard';
-import { AdminDashboard } from '@/components/admin/AdminDashboard';
+import { StudentDashboard } from '@/components/Students/StudentDashboard';
+import { TeacherDashboard } from '@/components/Teachers/TeacherDashboard';
+import { AdminDashboard } from '@/components/Admin/AdminDashboard';
 import { RoleRoute } from '../Authentication/RoleRoute';
 
 export const MainLayout = () => {
